a5acee73d781647de124a1799cf82572546e8963
Alena Dziomina <alenadziomina@gmail.com>
2021-04-16 15:32:01 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
