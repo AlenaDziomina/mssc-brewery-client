1ec4bdfc966b1e086bd6b08b8ff5257ef317fb45
Alena Dziomina <alenadziomina@gmail.com>
2021-04-16 15:41:47 +0200

    Model

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/model/BeerDto.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbreweryclient.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
